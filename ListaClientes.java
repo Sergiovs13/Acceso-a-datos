c43e88708fb66852b176c660fe5989fe724892ee
Sergio <Sergio@DESKTOP-F53ALD5>
2021-10-29 16:11:54 +0200

    Clase listaCLientes creada

---
+++ ListaClientes.java
@@ -0,0 +1,14 @@
+package ejerciciosFicherosObj;
+
+import javax.swing.*;
+
+public class ListaClientes extends JFrame{
+	
+	public ListaClientes() {
+		iniciarComponentes();
+	}
+	
+	public void iniciarComponentes() {
+		
+	}
+}
