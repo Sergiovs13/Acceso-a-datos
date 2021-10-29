bce6e9705b952f22c06d814c6fc0dec759b9ef1c
windows10 <windows10@SergioDam2>
2021-10-29 14:05:30 +0200

    ventana creada

--- AgregarClientesFile.java
+++ AgregarClientesFile.java
@@ -1,5 +1,49 @@
 package ejerciciosFicherosObj;
 
-public class AgregarClientesFile {
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.*;
+import java.io.*;
 
+public class AgregarClientesFile extends JFrame{
+
+	public AgregarClientesFile() {
+		iniciarComponentes();
+	}
+	
+	private void iniciarComponentes() {
+		setTitle("Agregar Clientes");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		
+		JLabel lblTexto = new JLabel("Agregar Clientes");
+		lblTexto.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblTexto.setBounds(158, 11, 174, 24);
+		getContentPane().add(lblTexto);
+		
+		JButton btnElegirFile = new JButton("Elegir Fichero");
+		btnElegirFile.setBounds(168, 46, 121, 23);
+		getContentPane().add(btnElegirFile);
+		btnElegirFile.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				abrirExploradorFicheros();
+			}
+		});
+		
+		setVisible(true);
+	}
+	
+	private void abrirExploradorFicheros() {
+		File f=null;
+		JFileChooser fc=new JFileChooser();
+		fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int opcion=fc.showOpenDialog(this);
+		
+		if(opcion==JFileChooser.APPROVE_OPTION) {
+			
+		}
+	}
 }
